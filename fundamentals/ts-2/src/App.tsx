27459398f0d75b617ee6f04268eb4d629f62e2fe
Zaeem Tarrar <47736272+ZaeemTarrar@users.noreply.github.com>
2021-06-05 07:41:51 +0500

    reactjs 0.0.1

---
+++ fundamentals/ts-2/src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import './App.css';
+
+interface Props {}
+
+const App: FC<Props> = (): React.ReactElement => {
+	return <div id="wrapper" />;
+};
+
+export default App;
