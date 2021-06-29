66ac2b73b6075b4139b25823e72a171b32a90816
Zaeem Tarrar <47736272+ZaeemTarrar@users.noreply.github.com>
2021-06-30 01:17:11 +0500

    reactjs 0.0.1

--- fundamentals/ts-4/src/App.tsx
+++ fundamentals/ts-4/src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/App.css'
+import React from 'react'
 import Notes from './components/function/Notes/Notes'
 import { Container } from 'react-bootstrap'
 
